cb5f24cc6ccb3fa0e5c11d38dfe544aaaa4dd65a
Safayet Mahid <safayetmahid96@gmail.com>
2022-02-01 02:02:46 +0600

    picture is added cursor functuinality and typewriter effect

--- main.js
+++ main.js
@@ -156,3 +156,55 @@ const sendEmail = () => {
     alert(`Please send me an email directly using the "write me ->" button from the email section!`)
 }
 
+// ====================================
+let wrapper
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+async function writingAll(stringTarget, container) {
+    wrapper = document.querySelector('[' + container + ']')
+    const stringsContainer = document.getElementsByClassName(stringTarget)
+
+    while (wrapper) {
+        for (i = 0; i < stringsContainer.length; i++) {
+            const string = stringsContainer[i].textContent
+            await write(string)
+            await sleep(1000)
+            await erase()
+            await sleep(1000)
+        };
+    }
+};
+
+async function write(text) {
+    let index = 0
+    while (index < text.length) {
+        const timeout = 100
+        await sleep(timeout)
+        index++
+        wrapper.innerHTML = text.substring(0, index)
+    }
+};
+
+
+async function erase() {
+    while (wrapper.textContent.length) {
+        const timeout = 100
+        await sleep(timeout)
+        wrapper.textContent = wrapper.textContent.substring(0, wrapper.textContent.length - 2)
+    }
+};
+
+writingAll('item', 'data-text');
+
+
+
+// ==============cursor===================
+
+
+const cursor = document.getElementById("cursor");
+
+document.onmousemove = function (e) {
+    cursor.style.left = (e.pageX - 25) + "px";
+    cursor.style.top = (e.pageY - 25) + "px";
+    cursor.style.display = "block"
+}
