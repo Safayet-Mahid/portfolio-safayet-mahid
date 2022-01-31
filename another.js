953c277da3619e3bd6e24990236cb061d64a9b7a
Safayet Mahid <safayetmahid96@gmail.com>
2022-01-31 13:22:33 +0600

    project is furnished with relevent information

--- another.js
+++ another.js
@@ -91,3 +91,31 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 400,
+    // reset:true
+})
+
+sr.reveal(`.section__subtitle`, { delay: 400 })
+sr.reveal(`.section__title`, { delay: 400 })
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, { delay: 700 })
+sr.reveal(`.home__social,.home__scroll`, { delay: 900, origin: "bottom" })
+sr.reveal(`.about__img`, { delay: 500, origin: "left" })
+sr.reveal(`.about__data`, { delay: 700, origin: "right" })
+sr.reveal(`.about__description`, { delay: 1000, origin: "bottom" })
+sr.reveal(`.first`, { delay: 700, origin: "left" })
+sr.reveal(`.second`, { delay: 700, origin: "right" })
+sr.reveal(`.skills__box`, { delay: 1500, origin: "bottom" })
+sr.reveal(`.services__card`, { delay: 700, origin: "bottom" })
+sr.reveal(`.work__filters`, { delay: 600, origin: "left" })
+sr.reveal(`.work__container`, { delay: 700, origin: "bottom" })
+sr.reveal(`.swiper`, { delay: 600, origin: "bottom" })
+sr.reveal(`.left`, { delay: 700, origin: "left" })
+sr.reveal(`.right`, { delay: 700, origin: "right" })
